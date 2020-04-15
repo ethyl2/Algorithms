705945fe232c2e0309d2d1d30fe770a74aa65225
Heather Nuffer <zzzuzz@gmail.com>
2020-04-14 18:06:02 -0600

    Adds solution for recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,27 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # First arg is recipe
+    # Second arg is total ingredients available
+    # Return num of batches possible, given the ingredients available.
+    batches_possible_arr = []
+    # Loop through ingredients to see how many batches can be made, considering that ingredient.
+    # Return the lowest batch number.
+    for ingredient in recipe.keys():
+        if ingredient in ingredients.keys():
+            batches_possible_arr.append(
+                ingredients[ingredient] // recipe[ingredient])
+        else:
+            return 0
+    return min(batches_possible_arr)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 138, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
