3d8d92b52204eb7c4b89003ee284e9b85ba17a3f
Heather Nuffer <zzzuzz@gmail.com>
2020-07-16 19:13:44 -0600

    Modifies eating_cookies to take out unnecessary base cases

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -24,12 +24,6 @@ def eating_cookies(n, cache=None):
         return cache[n]
     elif n <= 0:
         return 1
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    elif n == 3:
-        return 4
     else:
         result = eating_cookies(
             n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
